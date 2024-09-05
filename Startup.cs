c78702a93e04e424dcabea99e3b9f4f1a683da41
shraddhatapake <shraddhatapake20@gmail.com>
2024-09-06 00:53:44 +0530

    Add files via upload

---
+++ Startup.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RegistrationpageMVC
+{ public class startup
+    {
+        public startup(IConfiguration configuration)
+        {
+            configuration = configuration;
+        }
+        public IConfiguration Configuration { get; }
+        public void ConfigureServices(IServiceCollection services)
+        { services.AddControllersWithViews(); }
+        public void configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/home/error");
+                app.UseHsts();
+            }
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+            app.UseRouting();
+            app.UseAuthorization();
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllerRoute(
+                    name:"default",
+                    pattern:"{controller=Login}/{action=login}/{id}");
+            });
+        }
+    }
+}
+
+
+
+                
+        
+
+        
